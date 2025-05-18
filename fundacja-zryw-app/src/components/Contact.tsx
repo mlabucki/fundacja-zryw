d6b5e3974701cb7aeb686c36588db15d336abccd
Michał Łabucki <labucki.michal@gmail.com>
2025-05-18 13:53:52 +0200

    resuable Contaxt-section component

--- fundacja-zryw-app/src/components/Contact.tsx
+++ fundacja-zryw-app/src/components/Contact.tsx
@@ -1,27 +1,51 @@
 import React from "react";
-import people from "/photos/people.jpg";
 
+interface ContactProps {
+  text: string;
+  src?: string;
+  buttonText?: string;
+  onButtonClick?: () => void;
+  className?: string;
+  aspect?: string;
+}
 
-
-const Contact: React.FC = () => {
-  return (
-    <section className={`pb-32`}>
-        <div className="w-main mx-auto">
-            <img src={people} alt="" />
-
-            <div className="relative z-10 h-full flex flex-col justify-between p-12 md:p-20">
-                <p className="text-white text-center">
-                    Aplikuj na zryw
-                </p>
-                <div className="flex justify-center">
-                    <button>
-                        Aplikuj
-                    </button>
-                </div>
-            </div>
-        </div>
-    </section>
-  );
-};
+const Contact: React.FC<ContactProps> = ({
+  text,
+  src = "./photos/apply-to-zryw.jpg",
+  buttonText = "Aplikuj",
+  onButtonClick,
+}) => (
+  <div className={"w-full flex flex-col items-center"}>
+    <div className={`relative w-full rounded-[2px] overflow-hidden`}>
+      <img
+        src={src}
+        alt=""
+        className="w-full h-full object-cover rounded-[2px]"
+      />
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <h2 className="font-['New_Order'] text-white text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+          {text}
+        </h2>
+      </div>
+      {/* Prostokąt + półkole na desktopie */}
+      <div className="hidden md:flex absolute left-1/2 bottom-0 -translate-x-1/2 w-75 h-45 bg-white rounded-t-full shadow-md justify-center items-center z-10">
+        <button
+          className="bg-[var(--color-accent)] text-white font-['New_Order'] text-base px-8 py-3 rounded-full "
+          onClick={onButtonClick}
+        >
+          {buttonText}
+        </button>
+      </div>
+      <div className="flex md:hidden absolute left-1/2 bottom-4 -translate-x-1/2 z-10">
+        <button
+          className="bg-[var(--color-accent)] text-white font-['New_Order'] text-base px-8 py-3 rounded-full "
+          onClick={onButtonClick}
+        >
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default Contact;

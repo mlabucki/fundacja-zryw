c557cc14bb39b2fd2cc6db4f60a8af19004e197c
Dominik Dudek <dom-dud774@o2.pl>
2025-05-19 03:39:43 +0200

    Footer completed for desktop and mobile, prepared main content for home page, almost completed for desktop

--- fundacja-zryw-app/src/components/Footer.tsx
+++ fundacja-zryw-app/src/components/Footer.tsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 const Footer: React.FC = () => {
     const data = {
         NIP: '7011177639',
+        '': '',
         'E-mail': 'kontakt@fundacjazryw.pl',
         REGON: '527088981',
         Telefon: '+48 575 934 017',
@@ -12,30 +13,33 @@ const Footer: React.FC = () => {
     }
 
   return (
-    <footer className="h-footer bg-green">
-        <section className="w-main mx-auto flex justify-between mt-12">
-            <div className="">
-                <div className="flex"> 
+    <footer className="h-footer-mobile xs:h-footer bg-green">
+        <section className="w-mobile xs:w-main mx-auto flex xs:flex-row flex-col justify-between mt-7 xs:mt-12">
+            <div className="max-w-[410px]">
+                <div className="w-full flex justify-between xs:grid xs:grid-cols-2 xs:gap-x-40 xs:gap-y-12">
                     <NavLink to='/' className=''>
                         <p className='font-calluna text-xl text-white'>Strona główna</p>
                     </NavLink>
-                    <NavLink to='/about' className=''>
+                    <NavLink to='/about'>
                         <p className='font-calluna text-xl text-white'>O nas</p>
                     </NavLink>
-                    <NavLink to='/apply' className=''>
+                    <NavLink to='/apply'>
                         <p className='font-calluna text-xl text-white'>Aplikuj</p>
                     </NavLink>
                 </div>
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-40 gap-y-6 xs:gap-y-12 mt-6 xs:mt-12">
                     {Object.entries(data).map(([key, value]) => (
-                        <div key={key}>
-                            <div className="font-calluna text-base text-white">{key}</div>
-                            <div className="font-calluna text-base text-white">{value}</div>
-                        </div>
+                    <div
+                        key={key}
+                        className={key === '' ? "hidden xs:block" : ""}
+                    >
+                        <div className="font-calluna text-base text-white">{key}</div>
+                        <div className="font-calluna text-base text-white">{value}</div>
+                    </div>
                     ))}
                 </div>
             </div>
-            <img src={logo} alt="" />
+            <img src={logo} alt="" className="w-full xs:w-auto mt-6 xs:mt-0"/>
         </section>
     </footer>
   );
---
+++ fundacja-zryw-app/src/components/LandingHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import picture from '/photos/main-home.jpg'
+
+const LoadingHeader: React.FC = () => {
+  return (
+    <section className="relative w-mobile xs:w-main mx-auto mt-8 h-[672px] overflow-hidden">
+        <img src={picture} alt="zryw-main-picture" className="mt-[-108px]"/>
+        <div className="absolute inset-0 bg-[linear-gradient(285.13deg,_rgba(0,_0,_0,_0)_0.22%,_rgba(81,_81,_61,_0.2)_100%)]"></div>
+        <h1 className="absolute top-10 left-10 text-white text-8xl font-new-order leading-[0.95] tracking-[-0.03em]">Budujemy <br /> Nowe <br /> Kadry</h1>
+        <div
+            className="w-[338px] h-[187px] absolute bottom-10 right-10 bg-white rounded-lg p-6 flex flex-col justify-between"
+            style={{
+                WebkitMaskImage: `radial-gradient(circle 33px at 78% 100%, transparent 33px, black 34px)`,
+                maskImage: `radial-gradient(circle 33px at 78% 100%, transparent 33px, black 34px)`
+            }}
+        >
+            <p className="text-lg font-calluna">Jesteśmy społecznością państwowców.</p>
+            <p className="text-lg font-calluna">Pomagamy młodym osobom wejść w służbę publiczną i życie polityczne.</p>
+            <p className="text-lg font-calluna">Zobacz Więcej</p>
+        </div>
+    </section>
+  );
+};
+
+export default LoadingHeader;
